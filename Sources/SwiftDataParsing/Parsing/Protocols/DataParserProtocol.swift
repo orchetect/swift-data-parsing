a409e18562f91e38c26bc210a77b78786f661bd3
Steffan Andrews <stef@orchetect.com>
2026-03-05 22:39:56 -0800

    Updated inline docs

--- Sources/SwiftDataParsing/Parsing/Protocols/DataParserProtocol.swift
+++ Sources/SwiftDataParsing/Parsing/Protocols/DataParserProtocol.swift
@@ -21,17 +21,19 @@ public protocol DataParserProtocol {
     var remainingByteCount: Int { get }
     
     /// Advance (positive integer) or recede (negative integer) by the specified number of bytes from current read offset.
+    ///
+    /// If the resulting read offset is past the start or past one-past-the-end, an error is thrown.
     mutating func seek(by delta: Int) throws(DataParserError)
     
     /// Return the next byte and optionally increment the read offset.
     ///
-    /// If no bytes remain, an error will be returned.
+    /// If no bytes remain, an error will be thrown.
     mutating func readByte(advance: Bool) throws(DataParserError) -> DataElement
     
     /// Return _n_ number of bytes and optionally increment the read offset.
     /// If `nil` byte count is passed, the remainder of the data will be returned.
     ///
-    /// If fewer bytes remain than are requested, an error will be returned.
+    /// If fewer bytes remain than are requested, an error will be thrown.
     mutating func read(bytes count: Int?, advance: Bool) throws(DataParserError) -> DataRange
     
     /// Resets read offset back to byte index 0.
@@ -44,21 +46,21 @@ public protocol DataParserProtocol {
 extension DataParserProtocol {
     /// Return the next byte and increment the read offset.
     ///
-    /// If no bytes remain, an error will be returned.
+    /// If no bytes remain, an error will be thrown.
     public mutating func readByte() throws(DataParserError) -> DataElement {
         try readByte(advance: true)
     }
     
     /// Return the remainder of the data and increment the read offset.
     ///
-    /// If fewer bytes remain than are requested, an error will be returned.
+    /// If fewer bytes remain than are requested, an error will be thrown.
     public mutating func read() throws(DataParserError) -> DataRange {
         try read(bytes: nil, advance: true)
     }
     
     /// Return the remainder of the data, optionally incrementing the read offset.
     ///
-    /// If fewer bytes remain than are requested, an error will be returned.
+    /// If fewer bytes remain than are requested, an error will be thrown.
     public mutating func read(advance: Bool) throws(DataParserError) -> DataRange {
         try read(bytes: nil, advance: advance)
     }
@@ -66,7 +68,7 @@ extension DataParserProtocol {
     /// Return _n_ number of bytes and increment the read offset.
     /// If `nil` byte count is passed, the remainder of the data will be returned.
     ///
-    /// If fewer bytes remain than are requested, an error will be returned.
+    /// If fewer bytes remain than are requested, an error will be thrown.
     public mutating func read(bytes count: Int?) throws(DataParserError) -> DataRange {
         try read(bytes: count, advance: true)
     }
