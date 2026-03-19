980a6aec4d2cdf0712a1162338299f005eb1228f
Steffan Andrews <stef@orchetect.com>
2026-03-18 18:02:53 -0700

    `DataProtocol`: `toInt8()`: Added defaulted `encoding` parameter

--- Sources/SwiftDataParsing/Conversion/DataProtocol+Int.swift
+++ Sources/SwiftDataParsing/Conversion/DataProtocol+Int.swift
@@ -22,20 +22,21 @@ extension DataProtocol {
 // MARK: - Int8
 
 extension DataProtocol {
-    /// Returns a Int8 value from Data (stored as two's complement).
+    /// Returns an Int8 value from Data.
     /// Returns `nil` if Data is not the correct length.
     @_disfavoredOverload
-    public func toInt8() -> Int8? {
+    public func toInt8(from encoding: SignedIntegerEncoding = .signedBit) -> Int8? {
         guard count == 1 else {
             assertionFailure("Data byte length is incorrect. Expected 1 byte but got \(count).")
             return nil
         }
         
-        var int = UInt8()
-        withUnsafeMutableBytes(of: &int) {
+        var byte = UInt8()
+        withUnsafeMutableBytes(of: &byte) {
             _ = self.copyBytes(to: $0, count: 1)
         }
-        return Int8(bitPattern: int)
+        
+        return Int8(byte, encoding: encoding)
     }
 }
 
